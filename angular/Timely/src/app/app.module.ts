7fb8192cc321e507bcf4193a1c37c79d5014a1ac
Zdravko Pašalić <72557820+zdpasali@users.noreply.github.com>
2022-04-12 17:03:26 +0200

    creating components

--- angular/Timely/src/app/app.module.ts
+++ angular/Timely/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProjectlistComponent } from './projectlist/projectlist.component';
+import { ShowListComponent } from './projectlist/show-list/show-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectlistComponent,
+    ShowListComponent
   ],
   imports: [
     BrowserModule,
