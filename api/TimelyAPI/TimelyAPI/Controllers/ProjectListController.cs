c222ba7b774f16e7d7f0542173136e5778fce030
Zdravko Pašalić <72557820+zdpasali@users.noreply.github.com>
2022-04-12 14:42:25 +0200

    implementing get method

--- api/TimelyAPI/TimelyAPI/Controllers/ProjectListController.cs
+++ api/TimelyAPI/TimelyAPI/Controllers/ProjectListController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace TimelyAPI.Controllers
 {
@@ -18,5 +20,30 @@ namespace TimelyAPI.Controllers
         {
             _configuration = configuration;
         }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                    select ProjectName, StartTime, EndTime, DurationTime from dbo.ProjectList order by ProjectId";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("ProjectListAppConnection");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+
+                }
+            }
+            return new JsonResult(table);
+        }
+
     }
 }
