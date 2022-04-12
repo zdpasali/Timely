5c2154d2adf645f19782e59d8d189fc517f59330
Zdravko Pašalić <72557820+zdpasali@users.noreply.github.com>
2022-04-12 14:33:06 +0200

    adding controller

---
+++ api/TimelyAPI/TimelyAPI/Controllers/ProjectListController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace TimelyAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectListController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public ProjectListController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+    }
+}
