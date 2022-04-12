5d583de1cf0eae1972dc406da37bc5413eff86ec
Zdravko Pašalić <72557820+zdpasali@users.noreply.github.com>
2022-04-12 17:14:46 +0200

    editing services and adding api methods

--- angular/Timely/src/app/app.module.ts
+++ angular/Timely/src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProjectlistComponent } from './projectlist/projectlist.component';
 import { ShowListComponent } from './projectlist/show-list/show-list.component';
+import { SharedService } from './shared.service';
+
+import {HttpClientModule} from '@angular/common/http'
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,9 +18,12 @@ import { ShowListComponent } from './projectlist/show-list/show-list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [SharedService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- angular/Timely/src/app/shared.service.ts
+++ angular/Timely/src/app/shared.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SharedService {
 
-  constructor() { }
+  readonly APIUrl ="http://localhost:5000/api";
+
+  constructor(private http:HttpClient) { }
+
+  getProjectList():Observable<any[]>{
+    return this.http.get<any>(this.APIUrl+'/projectlist');
+
+  }
+
+  addToProjectList(val:any){
+    return this.http.post(this.APIUrl+'/projectlist',val);
+  }
 }
