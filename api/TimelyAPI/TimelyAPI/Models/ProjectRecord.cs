c50e3251fb9a959f127910aae0109b193579ecf8
Zdravko Pašalić <72557820+zdpasali@users.noreply.github.com>
2022-04-12 14:26:50 +0200

    adding class and database conection string

---
+++ api/TimelyAPI/TimelyAPI/Models/ProjectRecord.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimelyAPI.Models
+{
+    public class ProjectRecord
+    {
+        public string ProjectName { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+        public string DurationTime { get; set; }
+    }
+}
